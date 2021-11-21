de1ba9c7ddd55914a22c92c038d2cd4335a1fb5f
Igor Kind <86460417+ngessda@users.noreply.github.com>
2021-11-22 06:24:01 +0900

    none

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,23 +8,35 @@ namespace ConsoleApp1
 {
     class Program
     {
+        public static bool IsPrime(int n)
+        {
+            bool result = false;
+            int[] arr = new int[4] { 2, 3, 5, 7 };
+            if (n <= 1)
+            {
+                return result;
+            }
+            if (n == 2 || n == 3 || n == 5 || n == 7)
+            {
+                result = true;
+                return result;
+            }
+            for (int i = 1; i < 4; i++)
+            {
+                if (n % arr[i] == 0)
+                {
+                    return result;
+                }
+            }
+            result = true;
+            return result;
+            // TODO
+        }
         static void Main(string[] args)
         {
             Console.Write("Введие число для проверки: ");
             int n = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(zxc(n));
-            bool zxc(int x)
-            {
-                if (n <= 2 || n % 2 == 0)
-                {
-                    return n == 2;
-                }
-                for (int i = 3; i < Math.Sqrt(n); i += 2)
-                {
-                    if (n % i == 0) { return false; }
-                }
-                return true;
-            }
+            Console.WriteLine(IsPrime(n));
         }
     }
 }
